68955e4825142d4d9c6c8a8048db135475e336ea
Bob Zhao <fireheart54@gmail.com>
2019-07-17 09:45:11 -0400

    Linked list partition

---
+++ Chapter 2/Q4.py
@@ -0,0 +1,26 @@
+from LinkedList import LinkedList,Node
+
+def partition(ll, val):
+    before_list = LinkedList()
+    after_list = LinkedList()
+
+    current = ll.head
+    while current:
+        if current.value < val:
+            before_list.add(current.value)
+        else:
+            after_list.add(current.value)
+        current = current.next
+    print(before_list)
+    print(after_list)
+    if before_list.tail is None:
+        return after_list
+    else:
+        before_list.add(Node(None))
+        before_list.tail.next = after_list.head
+        return before_list
+
+ll = LinkedList()
+ll.generate(10, 0, 99)
+print(ll)
+print(partition(ll, ll.head.value))
