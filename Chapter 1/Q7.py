ac1c27a6e02efcfde451f11ac7d39154fa5c209b
Bob Zhao <fireheart54@gmail.com>
2019-07-30 20:54:47 -0400

    Matrix rotation

--- Chapter 1/Q7.py
+++ Chapter 1/Q7.py
@@ -1,4 +1,19 @@
 def rotate_matrix(matrix):
+    n = len(matrix)
+    for layer in range(n//2):
+        first = layer
+        last = n-layer-1
 
-    for layer in range(len(matrix[0])):
-        matrix_top = matrix[layer][layer][1:-1]
+        for i in range(first,last):
+            matrix_top = matrix[layer][i]
+
+            # left -> top
+            matrix[layer][i] = matrix[-i-1][layer]
+
+            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
+
+            matrix[-layer-1][-i-1] = matrix[i][-layer-1]
+
+            matrix[i][-layer-1] = top
+
+        return matrix
